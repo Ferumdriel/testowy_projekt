5f7a90e8480b21d2a5683a9a3ce6467811fb75e5
Mateusz Bieńkowski <ferumdriel@gmail.com>
2018-03-25 13:32:16 +0200

    updated println

--- src/TempAvenger.java
+++ src/TempAvenger.java
@@ -1,6 +1,6 @@
 public class TempAvenger {
 
     public static void main(String[] args) {
-        System.out.println("This is first addition to code");
+        System.out.println("This is the second addition");
     }
 }
